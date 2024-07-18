ab525e7efc64c80b90e6c495af85f73753785b91
Kennan kibet <167380832+Abdiwalibetto@users.noreply.github.com>
2024-07-18 19:28:51 +0300

    Add files via upload

---
+++ class W/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Person myName = new Person("Kennan Kibet", 21, "123 Main St");
+        myName.printCurrentInstance();
+    }
+}
---
+++ class W/Person.java
@@ -0,0 +1,25 @@
+public class Person {
+    private String name;
+    private int age;
+    private String address;
+
+    public Person(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Address: " + address);
+    }
+
+    public void myDetails(Person person) {
+        person.displayDetails();
+    }
+
+    public void printCurrentInstance() {
+        myDetails(this);
+    }
+}
